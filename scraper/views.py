e8df24541401d743e5560176652dad00388f45f4
chandlerprevatt21 <chandler@Chandlers-MBP.attlocal.net>
2021-12-18 16:48:15 -0600

    home update

--- scraper/views.py
+++ scraper/views.py
@@ -3,13 +3,11 @@ from bs4 import BeautifulSoup
 from django.template.loader import render_to_string
 
 def soup(request):
-    page = render_to_string('georgia.html')
+    page = render_to_string('ga-4.html')
     soup = BeautifulSoup(page, 'html.parser')
-    cells = soup.find_all(class_="_1Q9if")
-    span = soup.span()
+    cells = soup.find_all(class_="kn-detail-body")
     context = {
         'cells': cells,
-        'span': span
     }
     return render(request, 'scraper.html', context)
 
