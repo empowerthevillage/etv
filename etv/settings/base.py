182a3723a00d531f4f6d9f8466258c9e19e3c445
chandlerprevatt21 <chandler@Chandlers-MBP.attlocal.net>
2021-12-20 11:59:26 -0600

    update

--- etv/settings/base.py
+++ etv/settings/base.py
@@ -37,7 +37,7 @@ braintree_public = env('PUBLIC_KEY')
 braintree_private = env('PRIVATE_KEY')
 GATEWAY = braintree.BraintreeGateway(
     braintree.Configuration(
-        braintree.Environment.Sandbox,
+        braintree.Environment.Production,
         merchant_id = braintree_merchant_id,
         public_key = braintree_public,
         private_key = braintree_private,
