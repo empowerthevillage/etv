5a819b2241d61f20d9e8bb399ac93f6d8e4a4595
chandlerprevatt21 <chandlerprev@gmail.com>
2022-05-18 17:02:19 -0500

    update

--- billing/urls.py
+++ billing/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from .views import *
 
 urlpatterns = [
-    path('get_disbursement', braintree_disbursement, name='disbursement'),
+    path('get-disbursement/', braintree_disbursement, name='disbursement'),
 ]
