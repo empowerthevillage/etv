9a514822bad5b4336b84902cc6e70eb0167f16c9
chandlerprevatt21 <chandlerprev@gmail.com>
2022-11-08 11:21:09 -0800

    event update

--- carts/admin.py
+++ carts/admin.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 from .models import *
 from accounts.admin import admin_site
 
+class ArtCartAdmin(admin.ModelAdmin):
+    list_display = ['pk','timestamp', 'updated', 'active']
+    
 admin_site.register(Cart)
-admin_site.register(FullGalleryCart)
+admin_site.register(FullGalleryCart, ArtCartAdmin)
 admin_site.register(cartItem)
 admin_site.register(TicketCart)
 admin_site.register(ticketItem)
