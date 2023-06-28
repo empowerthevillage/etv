0267101a3f9a7a35346c5d190498f8fbcfa7ecb0
chandlerprevatt21 <chandlerprev@gmail.com>
2023-06-28 09:54:21 -0600

    update walkathon

--- walkathon/admin.py
+++ walkathon/admin.py
@@ -58,6 +58,27 @@ class WalkerDonationAdmin(admin.ModelAdmin):
         return response
     download_csv.short_description = "Download selected as csv"
     
+class OrgDonationAdmin(admin.ModelAdmin):
+    list_display = ['amount', 'organization', 'first_name','last_name', 'created']
+    actions = ['download_csv']
+    
+    def download_csv(self, request, queryset):
+        opts = queryset.model._meta
+        model = queryset.model
+        response = HttpResponse(content_type='text/csv')
+        # force download.
+        response['Content-Disposition'] = 'attachment;filename=export.csv'
+        # the csv writer
+        writer = csv.writer(response)
+        field_names = [field.name for field in opts.fields]
+        # Write a first row with header information
+        writer.writerow(field_names)
+        # Write data rows
+        for obj in queryset:
+            writer.writerow([getattr(obj, field) for field in field_names])
+        return response
+    download_csv.short_description = "Download selected as csv"
+    
 class PledgeAdmin(admin.ModelAdmin):
     list_display = ['amount', 'walker', 'created']
     
@@ -70,7 +91,7 @@ class RegistrationAdmin(admin.ModelAdmin):
     
 admin_site.register(Walker, WalkathonAdmin)
 admin_site.register(WalkerDonation, WalkerDonationAdmin)
-admin_site.register(OrgDonation)
+admin_site.register(OrgDonation, OrgDonationAdmin)
 admin_site.register(Sponsorship, SponsorAdmin)
 admin_site.register(HomeGalleryImage)
 admin_site.register(Organization)
