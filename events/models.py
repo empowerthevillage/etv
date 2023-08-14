a67e8cae32fb5e5e8af4ba3c8821b79ec747495f
chandlerprevatt21 <chandlerprev@gmail.com>
2023-08-14 09:43:04 -0600

    art ordering update

--- events/models.py
+++ events/models.py
@@ -555,7 +555,7 @@ class FullGalleryItem(models.Model):
     class Meta:
         verbose_name = 'Juneteenth Art Show Item'
         verbose_name_plural = 'Juneteenth Art Show Items'
-        ordering = ['sold', 'artist', 'title']
+        ordering = ['-sold', 'artist', 'title']
 
     def __str__(self):
         return str(self.title)
