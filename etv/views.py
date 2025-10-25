6c1ad121c9e212657fff5f4fc68dce9ac98944ca
Wyatt Miller <millerwjr@gmail.com>
2025-10-25 14:03:04 -0600

    integration test

--- etv/views.py
+++ etv/views.py
@@ -5,65 +5,71 @@ from django.http import HttpResponse
 
 gateway = settings.GATEWAY_PUBLIC
 
+
 def sitemap(request):
-    return HttpResponse(open('etv/sitemap.xml').read(), content_type='text/xml')
+    return HttpResponse(
+        open("etv/sitemap.xml").read(), content_type="text/xml"
+    )
+
 
 def home_page(request):
     form = BusinessForm()
     context = {
-        'nomination_form': form,
-        'title':'Empower The Village',
-        'seo_description': 'Empower The Village, Inc. (ETV) is a data-driven nonprofit 501(c)(3) organization founded in 2018 to develop, guide and implement breakthrough, scalable strategies that empower Black people, businesses and community organizations to realize their full potential.'
+        "nomination_form": form,
+        "title": "Empower The Village",
+        "seo_description": "Empower The Village, Inc. (ETV) is a data-driven nonprofit 501(c)(3) organization founded in 2018 to develop, guide and implement breakthrough, scalable strategies that empower Black people, businesses and community organizations to realize their full potential.",
     }
     return render(request, "home copy.html", context)
 
+
 def about_page(request):
-    context = {
-    'title':'ETV | About'
-    }
+    context = {"title": "ETV | About"}
     return render(request, "about.html", context)
 
+
+def integration_test_1(request):
+    context = {"title": "ETV | Integration Test 1"}
+    return render(request, "integration_test_1.html", context)
+
+
 def privacy(request):
-    context = {
-    'title':'ETV | Privacy Policy'
-    }
+    context = {"title": "ETV | Privacy Policy"}
     return render(request, "privacy.html", context)
 
+
 def terms(request):
-    context = {
-    'title':'ETV | Terms & Conditions'
-    }
+    context = {"title": "ETV | Terms & Conditions"}
     return render(request, "terms.html", context)
-    
+
+
 def strategic_pillars(request):
-    context = {
-    'title':'ETV | Strategic Pillars'
-    }
+    context = {"title": "ETV | Strategic Pillars"}
     return render(request, "strategic-pillars.html", context)
+
 
 def economic_prosperity(request):
     context = {
-        'title': 'ETV | Economic Prosperity & Employment',
+        "title": "ETV | Economic Prosperity & Employment",
     }
     return render(request, "prosperity.html", context)
 
+
 def news(request):
-    context = {
-    'title':'ETV | News'
-    }
+    context = {"title": "ETV | News"}
     return render(request, "news.html", context)
 
+
 def careers(request):
-    context = {
-    'title':'ETV | Careers'
-    }
+    context = {"title": "ETV | Careers"}
     return render(request, "careers.html", context)
+
 
 def shop(request):
     context = {
-        'title': 'ETV | Shop',
+        "title": "ETV | Shop",
     }
     return render(request, "shop.html", context)
 
+
 def robots(request):
-    return HttpResponse(open('etv/robots.txt').read(), content_type='text/txt')
+    return HttpResponse(open("etv/robots.txt").read(), content_type="text/txt")
