9ca44650bbbdfc874d70d74a7e45491f42274000
C-Wyatt.Miller <c-wyatt.miller@charter.com>
2026-08-27 15:07:27 -0600

    comment payment pathways

--- billing/urls.py
+++ billing/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('get-disbursement/', braintree_disbursement, name='disbursement'),
-    path('test-disbursement/', disbursement_test),
-    path('populate-transactions', populate_transactions)
+    path("get-disbursement/", braintree_disbursement, name="disbursement"),
+    # path('test-disbursement/', disbursement_test),
+    # path('populate-transactions', populate_transactions)
 ]
